6cd875a74c10e4bba592c949bd07a5a3229a00a3
Simo264 <60814604+Simo264@users.noreply.github.com>
2021-05-25 15:53:51 +0200

    Update main.cpp

--- Binary Tree/src/main.cpp
+++ Binary Tree/src/main.cpp
@@ -3,5 +3,12 @@
 
 int main()
 {
+    Binary_Tree Tree = create_node('A');
+    insert(Tree, create_node('B'));
+    insert(Tree, create_node('G'));
+    insert(Tree->left, create_node('C'));
+    insert(Tree->left, create_node('F'));
+    insert(Tree->right, create_node('D'));
+    insert(Tree->right, create_node('E'));
 
 }
